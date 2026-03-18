c3444272219009cb5d82fa96e76e038027beb894
Mohamed Jaffer <kjaffer69@gmail.com>
2026-03-18 11:50:04 +0530

    1st commit

---
+++ sample.java
@@ -0,0 +1,31 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println(" new Line ");
+        
+        System.out.println(" second Line ");
+        
+    }
+}
+
+class MyNewClass{
+    public void methodD(){
+        System.out.println("method D in MyNew Class");
+    }
+}
+
+class PrintingClass{
+    void printing(){
+        System.out.println("printing method in printing class");
+    }
+}
+
+class MyClass{
+    public void methodA(){
+        System.out.println("method A is reached in class MyClass");
+        System.out.println("added a new Line in the method A and class MyClass");
+    }
+}
+
+interface NewInterface{
+    
+}
